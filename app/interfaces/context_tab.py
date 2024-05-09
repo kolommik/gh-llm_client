b68cc7e711b252702c24240073f4a82a1f778a5f
kolommik <mikhail.kolomasov@gmail.com>
2024-05-09 12:57:43 +0300

    add dynamic file list into context_tab

--- app/interfaces/context_tab.py
+++ app/interfaces/context_tab.py
@@ -3,6 +3,7 @@ Implements the context tab in the Streamlit app, managing context files and disp
 """
 
 import streamlit as st
+import pandas as pd
 
 
 class ContextTab:
@@ -19,9 +20,51 @@ class ContextTab:
                 excluded_dirs=self.settings["excluded_dirs"],
             )
 
-            st.session_state["context"] = files
+            st.session_state["full_context"] = files
+            st.session_state["context"] = st.session_state["full_context"]
+
+            if "update_context_key" not in st.session_state:
+                st.session_state["update_context_key"] = 0
+            else:
+                st.session_state["update_context_key"] += 1
 
         if "context" in st.session_state:
+            update_context_key = (
+                st.session_state["update_context_key"]
+                if st.session_state["update_context_key"]
+                else 0
+            )
+
+            st.session_state["files_list"] = st.data_editor(
+                pd.DataFrame(
+                    [
+                        {
+                            "Path": item["path"],
+                            "Tokens": item["tokens"],
+                            "Lines": item["lines"],
+                            "Enable": True,
+                        }
+                        for item in st.session_state["full_context"]
+                    ]
+                ),
+                disabled=["path", "tokens", "lines"],
+                key=update_context_key,
+            )
+
+            enabled_paths = st.session_state["files_list"][
+                st.session_state["files_list"]["Enable"]
+            ]["Path"].tolist()
+
+            st.session_state["context"] = [
+                item
+                for item in st.session_state["full_context"]
+                if item["path"] in enabled_paths
+            ]
+
+            st.write(
+                "Total files:",
+                sum([1 for _ in st.session_state["context"]]),
+            )
             st.write(
                 "Total tokens:",
                 sum([x["tokens"] for x in st.session_state["context"]]),
@@ -29,17 +72,6 @@ class ContextTab:
             st.write(
                 "Total lines:",
                 sum([x["lines"] for x in st.session_state["context"]]),
-            )
-            st.table(
-                [
-                    {
-                        "path": item["path"],
-                        "tokens": item["tokens"],
-                        "length (symbols)": item["length"],
-                        "lines": item["lines"],
-                    }
-                    for item in st.session_state["context"]
-                ]
             )
 
         with st.expander("System prompt", expanded=False):
